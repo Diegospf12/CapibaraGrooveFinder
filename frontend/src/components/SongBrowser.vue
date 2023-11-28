9d0b9e1eea824a385d2aac3daf6c890feae35e55
Diegospf12 <94090499+Diegospf12@users.noreply.github.com>
2023-11-28 05:16:22 -0500

    Update SongBrowser.vue

--- frontend/src/components/SongBrowser.vue
+++ frontend/src/components/SongBrowser.vue
@@ -38,6 +38,13 @@
     </button>
   </form>
 
+  <div v-if="isSearching" class='flex space-x-2 justify-center items-center bg-white mt-20 dark:invert'>
+    <span class='sr-only'>Loading...</span>
+      <div class='h-8 w-8 bg-black rounded-full animate-bounce [animation-delay:-0.3s]'></div>
+    <div class='h-8 w-8 bg-black rounded-full animate-bounce [animation-delay:-0.15s]'></div>
+    <div class='h-8 w-8 bg-black rounded-full animate-bounce'></div>
+  </div>
+
   <p v-if="time" class="execution-time"> Tiempo de ejecución: {{ time }} ms </p>
 
   <transition-group name="fade" tag="div">
@@ -62,16 +69,19 @@ export default {
       tracks: [],
       time: null,
       option: false,
+      isSearching: false
     }
   },
   methods: {
     async search() {
+      this.isSearching = true;
       this.tracks = [];
       const optionText = this.option ? 'postgres' : 'myindex';
       const response = await fetch(`http://localhost:8000/search?query=${this.query}&k=${this.k}&option=${optionText}`)
       const data = await response.json()
       this.tracks = data.tracks
       this.time = data.execution_time
+      this.isSearching = false;
     }
   }
 }
